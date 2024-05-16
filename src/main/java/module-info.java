da74a2a7a3364e3befd599aab7bc6af41d0c8c06
Adrian Lopez <adrian.lopezm@campusfp.es>
2024-05-16 10:49:23 +0200

    Segundo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.empresa.datastructures_analysis {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
+    requires mongo.java.driver;
 
 
     opens com.empresa.datastructures_analysis to javafx.fxml;
